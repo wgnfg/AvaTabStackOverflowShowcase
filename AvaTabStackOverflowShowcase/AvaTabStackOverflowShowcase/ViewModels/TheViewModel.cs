10f6205a6a76c502e7443b9bf0e4fffa9179a2a0
wgnfg <96946778+wgnfg@users.noreply.github.com>
2025-06-01 21:46:45 +0800

    Add TheView(a No Header TabItem in it) and its ViewModel and put it into MainWindow

---
+++ AvaTabStackOverflowShowcase/AvaTabStackOverflowShowcase/ViewModels/TheViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaTabStackOverflowShowcase.ViewModels
+{
+    public class TheViewModel : ViewModelBase;
+        // If it dont inherit ViewModelBase,it will not work.
+        // CommunityTookit.MVVM is same.
+}
---
+++ AvaTabStackOverflowShowcase/AvaTabStackOverflowShowcase/Views/TheView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaTabStackOverflowShowcase.Views;
+
+public partial class TheView : UserControl
+{
+    public TheView()
+    {
+        InitializeComponent();
+    }
+}
